852865bc416ae9b9febe91b29c59feb4e4965794
Konstantin Láska <konstantin.laska@icloud.com>
2025-12-30 14:46:09 +0100

    Create deepsleep_test.ino

---
+++ SW/deepsleep_test/deepsleep_test.ino
@@ -0,0 +1,33 @@
+// Must be disabled USB CDC On Boot
+
+#define SLEEP_SEC 15         // Measurement interval (seconds)
+#define POWER 47
+
+void setup(void)
+{
+  Serial.begin(115200);
+  pinMode(POWER, OUTPUT); 
+  digitalWrite(POWER, HIGH); // enable power supply for ePaper and uSup
+  delay(500);
+
+  goToSleep();
+}
+
+void loop(void) 
+{
+
+}
+
+void goToSleep(){
+
+  Serial.println("I'm mot sleeping");
+  delay(1000);
+  Serial.println("going to sleep 15 sek");
+  digitalWrite(POWER, LOW); // disable power supply for ePaper
+  // ESP Deep Sleep 
+  Serial.println("ESP in sleep mode");
+  Serial.flush(); 
+  esp_sleep_enable_timer_wakeup(SLEEP_SEC * 1000000);
+  delay(200);
+  esp_deep_sleep_start();
+}
